3bf09b4a31defff8010aa91d12e727c4ade4ecaa
seif97-wecancity <saif.elfagal@wecancity.org>
2023-01-03 12:49:08 -0800

    first commit

---
+++ Demo.PL/Controllers/ComponentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.PL.Controllers
+{
+    public class ComponentsController : Controller
+    {
+        public IActionResult ComponentView()
+        {
+            return View();
+        }
+    }
+}
