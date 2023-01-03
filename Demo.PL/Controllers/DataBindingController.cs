c172e730a8414041524b899a747044f5eb22d72b
seif97-wecancity <saif.elfagal@wecancity.org>
2023-01-03 13:52:58 -0800

    second commit

---
+++ Demo.PL/Controllers/DataBindingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.PL.Controllers
+{
+    public class DataBindingController : Controller
+    {
+        public IActionResult Databindingview()
+        {
+            //this is the view data
+            ViewData["x"] = "hi , this is seif elafagal from the viewdata";
+
+            //this is the view bag & this is the most used 
+            ViewBag.s = "hey and i am from the viewbag";
+            ViewBag.array = "1,2,3,5,4,85";
+            return View();
+        }
+    }
+}
---
+++ Demo.PL/Controllers/FormsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.PL.Controllers
+{
+    public class FormsController : Controller
+    {
+        public IActionResult FormsView()
+        {
+            return View();
+        }
+    }
+}
